02a220dedb3e189142aff92bacd1c68a27cba91c
Mauricio Belforte <belymau@hotmail.com>
2024-09-30 04:57:58 -0300

    Agregada la funcionalidad del boton Gano

--- script.js
+++ script.js
@@ -11,13 +11,16 @@ class Jugador {
 
 let jugadores = [];
 
-document.getElementById('agregar').addEventListener('click', function () {
+const botonAgregar = document.getElementById('agregar')
+
+botonAgregar.addEventListener('click', function () {
     let nombre = document.getElementById('nombre').value;
     let puntos = parseInt(document.getElementById('puntos').value);
 
     if (nombre !== '') {
         let jugador = new Jugador(nombre, puntos);
         jugadores.push(jugador);
+        console.log(jugadores)
 
         actualizarListas();
         document.getElementById('nombre').value = '';
@@ -25,11 +28,32 @@ document.getElementById('agregar').addEventListener('click', function () {
     }
 });
 
-document.querySelectorAll('.gano').forEach(button => {
+
+
+const arrayConLos2BotonesGano = document.querySelectorAll('.gano')
+
+arrayConLos2BotonesGano.forEach(button => {
     button.addEventListener('click', function () {
+        /* document.getElementById("label-luchador1").textContent
+        let indice =  */
         let index = this.getAttribute('data-luchador') - 1;
         jugadores[index].sumarpunto();
+
+        if (index == 1) { //Esto es si gano el segundo luchador
+            //Mando al final al primero
+            console.log(jugadores)
+            const primerJugador = jugadores.shift();
+            jugadores.push(primerJugador)
+            console.log(primerJugador)
+            console.log(jugadores)
+        } else { // Esto es si gano el primero es decir con index==0   
+            const segundoJugador = jugadores.splice(1, 1)[0] // El [0] es porque splice esta devolviendo un arreglo con un solo elemento y con el [0] devuelve el elemento
+            jugadores.push(segundoJugador)
+            console.log(segundoJugador)
+            console.log(jugadores)
+        }
         actualizarListas();
+
     });
 });
 
